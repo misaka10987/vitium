56004aa909610a9695adf0a00db9984e15c1feda
matthew1115 <matthew_1115@outlook.com>
2024-01-03 20:09:15 +0800

    basic http connection built

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,10 +1,56 @@
-#include"httplib.h"
-#include<iostream>
+#include "httplib.h"
+#include <bits/stdc++.h> //very bad coding habit
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+
+class err_handle_pack
+{
+private:
+public:
+    int err_code;
+    string err_message;
+    err_handle_pack();
+    ~err_handle_pack();
+};
+
+err_handle_pack::err_handle_pack()
+{
+    err_code = 0;
+    err_message = "";
+}
+
+err_handle_pack::~err_handle_pack()
+{
+}
+
+// universal vaiables are declared here
+err_handle_pack err_package_major;
 
 int main()
 {
-    
+    string ip_address;
+    cout << "Please input your server IP : ";
+    cin >> ip_address;
+    try
+    {
+        httplib::Client cli(ip_address);
+        auto res = cli.Get("/user/init.json"); // this init should provide the list of player names and stuff, actual name depends on the server-side development (^w^)
+        if (res->status / 100 == 2)
+        {
+        }
+        else
+        {
+            throw res->status;
+        }
+        res->body;
+    }
+    catch (int e)
+    {
+        err_package_major.err_code = 20000 + e;
+        err_package_major.err_message = "HTTP connection initialize error ";
+        std::cerr << err_package_major.err_message;
+        std::cerr << e + 20000 << '\n';
+    }
 }
---
+++ client/error_code_list.md
@@ -0,0 +1,4 @@
+# Error codes for client
+### http errors
+all http errors will be added with 20000
+e.g. 404 NOT FOUND will be error 20404
