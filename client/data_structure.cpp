5bbcd10766472403a8c1e9cfc061d64c3b64894c
matthew1115 <matthew_1115@outlook.com>
2024-01-04 21:09:21 +0800

    introducing the json lib and data structure

--- client/data_structure.cpp
+++ client/data_structure.cpp
@@ -1,1 +1,18 @@
 #include<bits/stdc++.h>
+class trait
+{
+private:
+    int trait_num;
+    int* trait_pointer[1024];
+public:
+    trait(int num_of_traits,std::ifstream load);
+    ~trait();
+};
+
+trait::trait(int num_of_traits,std::ifstream load)
+{
+}
+
+trait::~trait()
+{
+}
