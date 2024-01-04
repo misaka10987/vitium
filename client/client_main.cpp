5f56c57eb7181740595b326fcbbd58c5714d557c
matthew1115 <matthew_1115@outlook.com>
2024-01-04 18:41:17 +0800

    minor changes

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -2,7 +2,8 @@
 // #define _WIN32_
 
 #include "httplib.h"
-#include <bits/stdc++.h> //very bad coding habit
+// #include <bits/stdc++.h> //very bad coding habit
+#include "data_structure.cpp" //<bits/stdc++> is already in this
 using std::cin;
 using std::cout;
 using std::endl;
@@ -10,7 +11,7 @@ using std::ifstream;
 using std::ofstream;
 using std::string;
 
-void clear()
+void clear() // a simple clear screen ...
 {
 #ifdef _WIN32_
     system("cls");
@@ -42,15 +43,42 @@ err_handle_pack::~err_handle_pack()
 
 // universal vaiables are declared here
 err_handle_pack err_package_major;
+ifstream local_r;
+ofstream local_w;
+string ip_address;
+
+void http_get(char server_path[], char local_path[])
+{
+    try
+    {
+        httplib::Client cli(ip_address);
+        auto res = cli.Get(server_path);
+        if (res->status / 100 == 2)
+        {
+        }
+        else
+        {
+            throw res->status;
+        }
+        if (!local_w.is_open())
+            local_w.open(local_path);
+        local_w << res->body; // load the data directly into the local file
+        local_w.close();
+    }
+    catch (int e)
+    {
+        err_package_major.err_code = 20000 + e;
+        err_package_major.err_message = "HTTP connection error ";
+        std::cerr << err_package_major.err_message;
+        std::cerr << e + 20000 << '\n';
+    }
+}
 
 int main()
 {
-    string ip_address;
     cout << "Please input your server IP : ";
     cin >> ip_address;
-    ifstream init_local_r;
-    ofstream init_local_w;
-    init_local_w.open(".init_local");
+    local_w.open(".local");
     clear();
     try
     {
@@ -63,14 +91,15 @@ int main()
         {
             throw res->status;
         }
-        init_local_w << res->body; // load the init directly into the local file
-        init_local_w.close();
+        local_w << res->body; // load the init directly into the local file
+        local_w.close();
     }
     catch (int e)
     {
-        err_package_major.err_code = 20000 + e;
+        err_package_major.err_code = 21000 + e;
         err_package_major.err_message = "HTTP connection initialize error ";
         std::cerr << err_package_major.err_message;
-        std::cerr << e + 20000 << '\n';
+        std::cerr << e + 21000 << '\n';
     }
+    std::cerr << "connection built to server." << '\n';
 }
--- client/error_code_list.md
+++ client/error_code_list.md
@@ -1,4 +1,6 @@
 # Error codes for client
 ### http errors
-all http errors will be added with 20000
+most http errors will be added with 20000
 e.g. 404 NOT FOUND will be error 20404
+http errors occuring at init stage will be added with 21000
+e.g. 404 will be 21404
