8782e70fcdf50c3ebdc5c148cbd0e6a10be35509
matthew1115 <matthew_1115@outlook.com>
2024-01-09 20:26:25 +0800

    helloworld and TUI basic windows built

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -13,14 +13,19 @@ char input_func()
 int main()
 {
     // init
-    // std::thread input_proc(input_func);
     const auto &main_win = cc::terminal::main_win;
     cc::terminal init;
     maxX = main_win.max_yx().second;
     maxY = main_win.max_yx().first;
     main_win << cc::format(0)("vitium");
     auto info_window{cc::widget::window{{1, 0, 15, maxX - 21}, cc::terminal::main_win}}; //{int curserY,int curserX,int heightY,int widthX}
+    auto stat_window{cc::widget::window{{1, maxX - 20, 15, 20}, cc::terminal::main_win}};
+    auto story_window{cc::widget::window{{17, 0, maxY - 17, maxX}, cc::terminal::main_win}};
+    auto buffer_window{cc::widget::window{{maxY, 0, 1, maxX}, cc::terminal::main_win}};
+    // std::thread input_proc(input_func);
     info_window << cc::format(2, 2)("HelloWorld from info-window!");
-    info_window.get_char();
+    stat_window << cc::format("Hello !", '\n', "From Stat");
+    story_window << cc::format(5)("Welcome to vitium client (under dev) !");
+    buffer_window.get_char();
     return 0;
 }
