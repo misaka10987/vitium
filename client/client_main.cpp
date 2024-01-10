bccfa43c9553cb09df306d0f5e63af23f6c1a433
matthew1115 <matthew_1115@outlook.com>
2024-01-10 16:41:57 +0800

    ip address basics

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,36 +1,56 @@
 #include <bits/stdc++.h> //very bad habit
 #include "cxxcurses/cxxcurses.hpp"
+#include <curses.h>
 namespace cc = cxxcurses;
 
 // global vars here
 int maxX, maxY;
+std::string ip_address;
 std::ostringstream my_input_stream;
+auto info_window{cc::widget::window{{1, 0, 15, maxX - 21}, cc::terminal::main_win}}; //{int curserY,int curserX,int heightY,int widthX}
+auto stat_window{cc::widget::window{{1, maxX - 20, 15, 20}, cc::terminal::main_win}};
+auto story_window{cc::widget::window{{17, 0, maxY - 17, maxX}, cc::terminal::main_win}};
+auto buffer_window{cc::widget::window{{maxY, 0, 1, maxX}, cc::terminal::main_win}};
 
-char input_func()
+void init()
 {
+    std::cout << "Server IP: ";
+    std::cin >> ip_address;
+}
+
+void input_func()
+{
+    char buff;
+    buff = wgetch(buffer_window.get());
+    if (buff != 'ERR')
+    {
+        my_input_stream << buff;
+    }
+}
+
+void window_refresh_all()
+{
+    info_window.refresh();
+    stat_window.refresh();
+    story_window.refresh();
+    buffer_window.refresh();
 }
 
 int main()
 {
-    // init
+    init();
     const auto &main_win = cc::terminal::main_win;
     cc::terminal init;
     maxX = main_win.max_yx().second;
     maxY = main_win.max_yx().first;
     main_win << cc::format(0)("vitium");
-    auto info_window{cc::widget::window{{1, 0, 15, maxX - 21}, cc::terminal::main_win}}; //{int curserY,int curserX,int heightY,int widthX}
-    auto stat_window{cc::widget::window{{1, maxX - 20, 15, 20}, cc::terminal::main_win}};
-    auto story_window{cc::widget::window{{17, 0, maxY - 17, maxX}, cc::terminal::main_win}};
-    auto buffer_window{cc::widget::window{{maxY, 0, 1, maxX}, cc::terminal::main_win}};
-    // std::thread input_proc(input_func);
+    main_win << cc::format(1, 1)("IP: " + ip_address);
+    std::thread input_proc(input_func);
     info_window << cc::format(1, 1)("HelloWorld from info-window!");
     stat_window << cc::format(1, 1)("Hello !"); // do not use \n to change line because you will lose part of the frame
     stat_window << cc::format(2, 1)("From Stat !");
     story_window << cc::format(5)("Welcome to vitium client (under dev) !");
-    info_window.refresh();
-    stat_window.refresh();
-    story_window.refresh();
-    buffer_window.refresh();
+    window_refresh_all();
     main_win.get_char();
     return 0;
 }
