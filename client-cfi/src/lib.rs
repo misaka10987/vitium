9a5db4f8cbc24989af0df57671d001633eaef242
Ca3PO42 <lucaserdong@qq.com>
2024-01-10 19:09:30 +0800

    Helloworld from client-cfi.

--- client-cfi/src/lib.rs
+++ client-cfi/src/lib.rs
@@ -1,18 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+#[no_mangle]
+pub extern "C" fn get(){
+    println!("Helloworld!");
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
-#[no-mangle]
-
-pub extern "C" fn 
