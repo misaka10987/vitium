8e10c945f8646bb1f55aaeb07cf768dc1973513c
Matthew He <matthew_1115@outlook.com>
2024-01-20 15:37:35 +0800

    removed some trash

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,21 +1,23 @@
 #include "cxxcurses/cxxcurses.hpp"
-#include<curses.h>
-#include<mutex>
-#include<iostream>
+#include <curses.h>
+#include <mutex>
+#include <iostream>
 namespace cc = cxxcurses;
 
 #define OS unix
 
 #if OS == unix
-#include<thread>
-void my_sleep(double seconds) {
+#include <thread>
+void my_sleep(double seconds)
+{
     int timetosleep;
     timetosleep = (int)(seconds * 1000);
     std::this_thread::sleep_for(std::chrono::milliseconds(timetosleep));
 }
 #elif OS == windows
 #include <Windows.h>
-void my_sleep(double seconds) {
+void my_sleep(double seconds)
+{
     Sleep(seconds * 1000);
 }
 #endif
@@ -24,23 +26,26 @@ void my_sleep(double seconds) {
 int maxX, maxY;
 std::string ip_address;
 std::stringstream my_input_stream;
-cc::widget::stdscr_wrapper* main_win;
-cc::widget::window* info_window;
-cc::widget::window* stat_window;
-cc::widget::window* story_window;
-cc::widget::window* buffer_window;
-cc::widget::window* life_window;
+cc::widget::stdscr_wrapper *main_win;
+cc::widget::window *info_window;
+cc::widget::window *stat_window;
+cc::widget::window *story_window;
+cc::widget::window *buffer_window;
+cc::widget::window *life_window;
 bool exit_loop = 0;
 std::mutex input_stream_lock;
 
-void init() {
+void init()
+{
     std::cout << "Server IP: ";
     std::cin >> ip_address;
 }
 
-void input_func() {
+void input_func()
+{
     char buff;
-    while (!exit_loop) {
+    while (!exit_loop)
+    {
         buff = getch();
         input_stream_lock.lock();
         my_input_stream << buff;
@@ -49,7 +54,8 @@ void input_func() {
     }
 }
 
-void window_refresh_all() {
+void window_refresh_all()
+{
     info_window->refresh();
     stat_window->refresh();
     story_window->refresh();
@@ -58,7 +64,8 @@ void window_refresh_all() {
     main_win->refresh();
 }
 
-void client_welcome_page() {
+void client_welcome_page()
+{
     *info_window << cc::format(1, 1)("HelloWorld from info-window!");
     *stat_window << cc::format(1, 1)("Hello !"); // do not use \n to change line because you will lose part of the frame
     *stat_window << cc::format(2, 1)("From Stat !");
@@ -66,7 +73,8 @@ void client_welcome_page() {
     window_refresh_all();
 }
 
-void main_loop() {
+void main_loop()
+{
     std::string buffer_storage;
     int counter = 0;
     bool got_data = 0;
@@ -75,29 +83,34 @@ void main_loop() {
         my_sleep(0.1);
         counter++;
     }
-    if (counter < 3) {
+    if (counter < 3)
+    {
         my_input_stream >> buffer_storage;
         input_stream_lock.unlock();
         got_data = 1;
     }
-    if (got_data) {
-        *buffer_window << cc::format(0, 0)(buffer_storage);
-    }
+    // if (got_data) {
+    //     *buffer_window << cc::format(0, 0)(buffer_storage);
+    // }
+
+    // TODO
+
     buffer_storage = "";
     window_refresh_all();
 }
 
-int main() {
+int main()
+{
     main_win = &cc::terminal::main_win;
     init();
     cc::terminal init;
     maxX = main_win->max_yx().second;
     maxY = main_win->max_yx().first;
-    auto _info_window{ cc::widget::window{{1, 0, 15, maxX - 21}, cc::terminal::main_win} }; //{int curserY,int curserX,int heightY,int widthX}
-    auto _stat_window{ cc::widget::window{{1, maxX - 20, 15, 20}, cc::terminal::main_win} };
-    auto _story_window{ cc::widget::window{{17, 0, maxY - 17, maxX}, cc::terminal::main_win} };
-    auto _buffer_window{ cc::widget::window{{maxY, 0, 1, maxX - 21}, cc::terminal::main_win} };
-    auto _life_window{ cc::widget::window{{maxY, maxX - 20, 1, 20}, cc::terminal::main_win} };
+    auto _info_window{cc::widget::window{{1, 0, 15, maxX - 21}, cc::terminal::main_win}}; //{int curserY,int curserX,int heightY,int widthX}
+    auto _stat_window{cc::widget::window{{1, maxX - 20, 15, 20}, cc::terminal::main_win}};
+    auto _story_window{cc::widget::window{{17, 0, maxY - 17, maxX}, cc::terminal::main_win}};
+    auto _buffer_window{cc::widget::window{{maxY, 0, 1, maxX - 21}, cc::terminal::main_win}};
+    auto _life_window{cc::widget::window{{maxY, maxX - 20, 1, 20}, cc::terminal::main_win}};
     info_window = &_info_window;
     stat_window = &_stat_window;
     story_window = &_story_window;
@@ -111,8 +124,6 @@ int main() {
     *main_win << cc::format(0, 1)(ip_address.c_str());
     std::thread input_proc(input_func);
     exit_loop = 0;
-    auto some_window{ cc::widget::window { { 10, 5, 5, 30 },*main_win } };
-    some_window << cc::format(2, 2)("Hello from sub-window!");
     window_refresh_all();
     client_welcome_page();
     while (!exit_loop)
