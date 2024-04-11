19180a70bb287f63c725661101af2b18cff54d7d
Ca3PO42 <lucaserdong@qq.com>
2024-04-11 10:59:12 +0800

    aaaaaa

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,6 +1,17 @@
 use cursive::views::TextView;
-use cursive::{Cursive, CursiveExt};
-pub fn addtxt(Obj:&mut Cursive,mes:String){
-    Obj.add_layer(TextView::new(mes));
-    Obj.run();
+use cursive::CursiveRunnable;
+pub fn init(obj:&mut CursiveRunnable,rd:String) -> bool{
+    let a = obj.load_theme_file(rd);
+    if a.is_err() {
+        return false;
+    }else{
+        a.unwrap();
+        return true;
+    }
+}
+pub fn addtxt(obj:&mut CursiveRunnable,mes:String){
+    obj.add_layer(TextView::new(mes));
+}
+pub fn setquit(obj:&mut CursiveRunnable,key:char){
+    obj.add_global_callback(key,|s| s.quit());
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,14 @@
-use cursive::views::TextView;
-use cursive::{Cursive, CursiveExt};
-use vitium_client::addtxt;
-fn main() {
-    let mut siv = Cursive::new();
+use vitium_client::{addtxt, setquit,init};
+fn main(){
+    let mut siv = cursive::default();
     let ss = &mut siv;
-    addtxt(ss,"Hello,world".to_string());
-    addtxt(ss,"aaaa".to_string());
+    init(ss,"style.toml".to_string());
+    addtxt(ss,"Hello,world. Press q to exit.".to_string());
+    setquit(ss,'q');
+    ss.run();
+    println!("Enter your message here:");
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    addtxt(ss,line);
+    ss.run();
 }
