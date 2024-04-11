f862abbf5a7d72e4d44520f6961cb2ef33bbee4e
misaka10987 <misaka10987@outlook.com>
2024-04-11 15:38:13 +0800

    go on

--- common/src/reg.rs
+++ common/src/reg.rs
@@ -1,14 +1,38 @@
-use std::collections::HashMap;
+use std::{borrow::Cow, collections::HashMap};
 
 use crate::ID;
 
-pub struct Reg<T>(HashMap<ID, T>);
+pub struct Reg<T> {
+    map: HashMap<ID, T>,
+    inv: HashMap<usize, ID>,
+}
 
 impl<T: Clone> Reg<T> {
     pub fn new() -> Self {
-        Self(HashMap::new())
+        Self {
+            map: HashMap::new(),
+            inv: HashMap::new(),
+        }
     }
-    pub fn id(&self, id: &ID) -> Option<&T> {
-        self.0.get(id)
+    pub fn inst(&self, ox: Ox<T>) -> Option<Cow<T>> {
+        match ox {
+            Ox::Reg(id) => self.map.get(&id).map(|r| Cow::Borrowed(r)),
+            Ox::Inst(p) => Some(Cow::Owned(*p)),
+        }
     }
+    pub fn save(&self, cow: &Cow<T>) -> Option<Ox<T>> {
+        match cow {
+            Cow::Borrowed(r) => {
+                let r: &T = r;
+                let p = r as *const T as usize;
+                self.inv.get(&p).map(|id| Ox::Reg(id.clone()))
+            }
+            Cow::Owned(t) => Some(Ox::Inst(Box::new(t.clone()))),
+        }
+    }
+}
+
+pub enum Ox<T> {
+    Reg(ID),
+    Inst(Box<T>),
 }
