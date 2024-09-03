be1e2ee7d8171fe8ae96cc52ca905bcc5f27010b
matthew1115 <matthew_1115@outlook.com>
2024-09-03 22:44:59 +0800

    screen size util

--- client/src/libs/ncurses_utils.hpp
+++ client/src/libs/ncurses_utils.hpp
@@ -1,1 +1,14 @@
-#include<ncurses.h>
+#include <ncurses.h>
+#include <utility>
+namespace nutils
+{
+    auto screen_size()
+    {
+        struct
+        {
+            int16_t x = getmaxx(stdscr);
+            int16_t y = getmaxy(stdscr);
+        } screen;
+        return screen;
+    }
+} // namespace nutils
