2680ee86ba62f6b3b4bb5ad89a71a478c00e62b3
matthew1115 <matthew_1115@outlook.com>
2024-09-05 20:57:11 +0800

    a minor fix on an include error in frontend.cpp

--- client/src/frontend.cpp
+++ client/src/frontend.cpp
@@ -1,2 +1,2 @@
 /// this file is meant to provide an interface and tools to show messeges on the screen, as well as maintain the vitium ui.
-#include "libs/curses_include.hpp"
+#include "libs/ncurses_utils.cpp"
