6baa5e3580a1bbc34facdc3e5919f7a2cf16c638
misaka10987 <misaka10987@outlook.com>
2024-10-11 20:13:42 +0800

    fix titlebar cursor style

--- client/src/script/base.ts
+++ client/src/script/base.ts
@@ -4,7 +4,7 @@ $(() => {
   const titlebar = `
 <div data-tauri-drag-region class='bg-stone-900 h-6 select-none absolute top-0 left-0 right-0 flex justify-end overflow-hidden'>
 <div data-tauri-drag-region id='title'
-  class='absolute top-0 left-0 right-0 h-6 justify-center text-white text-center align-middle truncate'>Vitium</div>
+  class='absolute top-0 left-0 right-0 h-6 justify-center text-white text-center align-middle truncate hover:cursor-default'>Vitium</div>
 <div class='titlebar-button hover:bg-zinc-500 z-50 justify-center max-h-6 h-6' id='titlebar-minimize'>
   <img class='max-h-6 h-6 p-1' src='/assets/svg/win-min.svg' alt='minimize'>
 </div>
