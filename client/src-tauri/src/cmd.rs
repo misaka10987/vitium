525274e69e7f794af04522ee1b847289c94ea0a1
misaka10987 <misaka10987@outlook.com>
2024-10-15 20:44:50 +0800

    add change password subcommand

--- client/src-tauri/src/cmd.rs
+++ client/src-tauri/src/cmd.rs
@@ -1,5 +1,6 @@
 use reqwest::Client;
-use vitium_api::net::{Req, SignUp};
+use tokio::runtime;
+use vitium_api::net::{EditPass, Req, SignUp};
 
 pub fn signup(user: &str, pass: &str, server: &str) -> anyhow::Result<()> {
     let req = SignUp {
@@ -9,9 +10,26 @@ pub fn signup(user: &str, pass: &str, server: &str) -> anyhow::Result<()> {
     let path = req.path();
     let url = format!("http://{server}{path}");
     let client = Client::new();
-    let run = tokio::runtime::Builder::new_current_thread()
+    let run = runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
     run.block_on(client.post(url).json(&req).send())?;
+    Ok(())
+}
+
+pub fn chpass(user: &str, old: &str, new: &str, server: &str) -> anyhow::Result<()> {
+    let req = EditPass(new.into());
+    let path = req.path();
+    let url = format!("http://{server}{path}");
+    let client = Client::new();
+    let run = runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
+    let future = client
+        .post(url)
+        .json(&req)
+        .basic_auth(user, Some(old))
+        .send();
+    run.block_on(future)?;
     Ok(())
 }
--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -3,19 +3,22 @@ mod chat;
 mod cmd;
 mod err;
 mod net;
+// mod respack;
 
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 use dirs::config_dir;
 use reqwest::Client;
-use tauri::generate_handler;
+use tauri::{generate_context, generate_handler};
 use tokio::sync::RwLock;
 
 use once_cell::sync::Lazy;
 use tracing::{info, Level};
 
 #[derive(Parser, Debug)]
+#[command(about, version)]
+#[command(after_help = "Launch the client GUI if none of the subcommands is provided.")]
 struct Args {
     /// Override configuration file.
     #[arg(short, long, default_value = config_dir().unwrap().join("vitium").join("client.toml").into_os_string())]
@@ -28,12 +31,29 @@ struct Args {
 enum Commands {
     /// Signup a new user on specified server.
     Signup {
-        #[arg(long)]
+        #[arg(short, long)]
+        /// Username.
+        user: String,
+        #[arg(short, long)]
+        /// Password.
+        pass: String,
+        /// Server address.
+        #[arg(value_name = "SERVER")]
+        server: String,
+    },
+    #[clap(name = "chpass")]
+
+    /// Change password for specified user on server.
+    ChPass {
+        #[arg(short, long)]
         /// Username.
         user: String,
         #[arg(long)]
-        /// Password.
-        pass: String,
+        /// Old assword.
+        old: String,
+        #[arg(long)]
+        /// New assword.
+        new: String,
         /// Server address.
         #[arg(value_name = "SERVER")]
         server: String,
@@ -73,6 +93,12 @@ pub fn run() -> anyhow::Result<()> {
     if let Some(c) = &ARG.cmd {
         return match c {
             Commands::Signup { user, pass, server } => cmd::signup(user, pass, server),
+            Commands::ChPass {
+                user,
+                old,
+                new,
+                server,
+            } => cmd::chpass(user, old, new, server),
         };
     }
     tracing_subscriber::fmt()
@@ -92,7 +118,7 @@ pub fn run() -> anyhow::Result<()> {
             chat::render_chat,
             chat::chat_modified,
         ])
-        .run(tauri::generate_context!())
+        .run(generate_context!())
         .expect("error while running tauri application");
     Ok(())
 }
