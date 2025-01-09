7f96a3f5d6061b160192afa774325312ffd055bc
misaka10987 <misaka10987@outlook.com>
2025-01-09 12:50:55 +0800

    stashed changes

--- api/src/game/attr.rs
+++ api/src/game/attr.rs
@@ -5,4 +5,4 @@ use fe3o4::def_regtab;
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Attr;
 
-def_regtab!(Attr, R_ATTR);
+def_regtab!(Attr);
--- api/src/prelude.rs
+++ api/src/prelude.rs
@@ -1,3 +1,6 @@
 pub use super::err::Error as ApiError;
 pub use super::player::Player;
 pub use super::uid::UId;
+pub mod reg {
+    pub use fe3o4::{HasRegTab, Id};
+}
