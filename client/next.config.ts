fa1bd4c9b66cc660dffcbcd4406f35e904acb6e2
misaka10987 <misaka10987@outlook.com>
2025-03-25 11:20:08 +0800

    init commit

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // webpack: (config,options) => {
+  //   console.log(options.webpack.version)
+  // }
 };
 
 export default nextConfig;
