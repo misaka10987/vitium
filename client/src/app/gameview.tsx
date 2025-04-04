51c862cc3bd26ae389d06a2812a27566c1ab8459
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-04-04 21:27:17 +0800

    cleanup

--- client/src/app/gameview.tsx
+++
@@ -1,9 +0,0 @@
-export function GameView({ username }: { username: string }) {
-  return (
-    <div className="game-container">
-      <div className="welcome-banner">
-        <h2>Welcome, {username}!</h2>
-      </div>
-    </div>
-  );
-}
