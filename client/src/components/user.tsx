271727a0d0ea86487412158870dfcde806eb59f4
misaka10987 <misaka10987@outlook.com>
2025-04-07 12:53:30 +0800

    format code

--- client/src/components/user.tsx
+++ client/src/components/user.tsx
@@ -1,13 +1,13 @@
 import { create } from "zustand"
 
 export const username = create<{
-    name?: string
-    setName: (name: string) => void
+  name?: string
+  setName: (name: string) => void
 }>()((set) => ({
-    setName: (username) => set(() => ({ name: username })),
+  setName: (username) => set(() => ({ name: username })),
 }))
 
 export const Username = () => {
-    const name = username(state => state.name)
-    return name
+  const name = username(state => state.name)
+  return name
 }
