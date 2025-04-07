8fa6d790ed3c0741dbf00c480172dccd155e535f
misaka10987 <misaka10987@outlook.com>
2025-04-07 20:29:54 +0800

    remove Next.js custom font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { NavBar } from "@/components/navbar"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Vitium",
@@ -26,9 +15,7 @@ export default function Layout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className='antialiased'>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
