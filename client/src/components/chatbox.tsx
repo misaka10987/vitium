62ded31772fa126a40d47d28cc898576eb1b31e6
misaka10987 <misaka10987@outlook.com>
2025-04-23 12:37:22 +0800

    improvements on chat

--- client/src/components/chatbox.tsx
+++ client/src/components/chatbox.tsx
@@ -72,63 +72,59 @@ export const Chatbox = () => {
 
   return (
     <div className="flex flex-col h-full w-full">
-      <div className="flex-1 flex flex-col">
-        <ScrollArea
-          className="h-[calc(100vh-180px)] flex w-full rounded-md border p-1 mb-2"
-          type="auto"
-          scrollHideDelay={100}
-        >
-          <div className="p-2">
-            {messages.map((msg, index) => (
-              <Chatbubble key={index} {...msg} />
-            ))}
-          </div>
-        </ScrollArea>
-        <form
-          ref={msgForm}
-          className="flex gap-2 items-center"
-          onSubmit={(e) => {
-            e.preventDefault()
-            const form = new FormData(e.currentTarget)
-            const msg = form.get('msg')?.toString()
-            if (msg == undefined) throw Error()
-            sendMessage(msg)
-            if (msgInput?.current == null) throw Error()
-            msgInput.current.value = ''
-          }}
-        >
-          <Textarea
-            ref={msgInput}
-            name="msg"
-            className="h-[90px] w-full resize-none overflow-auto py-2"
-            placeholder="Type your message here..."
-            required
-            onKeyDown={(e) => {
-              if (e.key === 'Enter' && !e.shiftKey) {
-                e.preventDefault()
-                msgForm.current?.requestSubmit()
-              }
-            }}
-          />
-          <div className="flex flex-col gap-1">
-            <Button
-              className="h-[40px] px-4"
-              type="submit"
-              aria-label="Send message"
-            >
-              <Send className="h-4 w-4" />
-            </Button>
-            <Button
-              className="h-[40px] px-4 mt-1"
-              type="button"
-              variant="outline"
-              aria-label="Upload image"
-            >
-              <Photo className="h-4 w-4" />
-            </Button>
-          </div>
-        </form>
+      <div className="h-[calc(100vh-180px)] flex w-full rounded-md border p-1 mb-2 overflow-scroll">
+        <div className="flex flex-col w-full">
+          {messages.map((msg, index) => (
+            <div className="flex w-full p-2" key={index}>
+              <Chatbubble {...msg} />
+            </div>
+          ))}
+        </div>
       </div>
+      <form
+        ref={msgForm}
+        className="flex gap-2 items-center"
+        onSubmit={(e) => {
+          e.preventDefault()
+          const form = new FormData(e.currentTarget)
+          const msg = form.get('msg')?.toString()
+          if (msg == undefined) throw Error()
+          sendMessage(msg)
+          if (msgInput?.current == null) throw Error()
+          msgInput.current.value = ''
+        }}
+      >
+        <Textarea
+          ref={msgInput}
+          name="msg"
+          className="h-[90px] w-full resize-none overflow-auto py-2"
+          placeholder="Type your message here..."
+          required
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+              e.preventDefault()
+              msgForm.current?.requestSubmit()
+            }
+          }}
+        />
+        <div className="flex flex-col gap-1">
+          <Button
+            className="h-[40px] px-4"
+            type="submit"
+            aria-label="Send message"
+          >
+            <Send className="h-4 w-4" />
+          </Button>
+          <Button
+            className="h-[40px] px-4 mt-1"
+            type="button"
+            variant="outline"
+            aria-label="Upload image"
+          >
+            <Photo className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
--- client/src/components/chatbubble.tsx
+++ client/src/components/chatbubble.tsx
@@ -20,47 +20,50 @@ export const Chatbubble = ({
   return (
     <div
       className={cn(
-        'flex flex-col gap-2',
-        variant === 'send' && 'self-end',
-        variant === 'receive' && 'self-start'
+        'flex flex-row gap-2 w-full',
+        variant == 'send' && 'flex-row-reverse'
       )}
     >
-      <div className="flex items-start gap-2">
-        <div className={cn('flex-shrink-0', variant === 'send' && 'order-2')}>
-          <Avatar className="m-1">
-            <AvatarFallback className="text-sm select-none">
-              {sender?.substring(0, 2)}
-            </AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex-grow">
-          <div
-            className={cn(
-              'flex items-center gap-1',
-              variant === 'send' && 'justify-end'
-            )}
+      <div className="flex">
+        <Avatar className="m-1">
+          <AvatarFallback className="text-sm select-none">
+            {sender?.substring(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex-grow">
+        <div
+          className={cn(
+            'flex flex-row gap-1',
+            variant === 'send' && 'flex-row-reverse'
+          )}
+        >
+          <Badge
+            variant="outline"
+            className="flex text-[10px] py-0 px-1 h-auto mt-1 bg-transparent text-muted-foreground border-border flex-shrink-0 select-none"
           >
-            <span className="text-sm font-medium mt-1 text-muted-foreground truncate select-none">
-              {sender}
-            </span>
-            <Badge
-              variant="outline"
-              className="text-[10px] py-0 px-1 h-auto mt-1 bg-transparent text-muted-foreground border-border flex-shrink-0 select-none"
-            >
-              {new Date(Number(time)).toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </Badge>
-          </div>
+            {new Date(Number(time)).toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </Badge>
+          <span className="flex text-sm font-medium mt-1 text-muted-foreground truncate select-none">
+            {sender}
+          </span>
+        </div>
+        <div
+          className={cn(
+            'flex',
+            variant == 'send' && 'justify-end',
+            variant == 'receive' && 'justify-start'
+          )}
+        >
           <div
             className={cn(
-              'text-sm p-1 pl-2 pr-2 mt-1 rounded-lg max-w-md break-all overflow-hidden w-fit',
-              variant === 'default' && 'bg-secondary text-secondary-foreground',
-              variant === 'send' &&
-                'bg-primary text-primary-foreground ml-auto rounded-tr-none',
-              variant === 'receive' &&
-                'bg-secondary text-secondary-foreground mr-auto rounded-tl-none'
+              'text-sm p-1 pl-2 pr-2 mt-1 rounded-lg max-w-md w-fit',
+              variant == 'default' && 'bg-secondary text-secondary-foreground',
+              variant == 'send' && 'bg-primary text-primary-foreground',
+              variant == 'receive' && 'bg-secondary text-secondary-foreground'
             )}
           >
             {renderedContent}
