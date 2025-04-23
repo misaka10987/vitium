ce99548a477357aa35c263764c69e6d2aa98a474
misaka10987 <misaka10987@outlook.com>
2025-04-23 11:42:57 +0800

    remove icon in navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -6,7 +6,6 @@ import {
   NavigationMenuList,
 } from '@/components/ui/navigation-menu'
 import { DarkModeSwitch } from '@/components/dark-mode-switch'
-import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
@@ -15,9 +14,8 @@ export const NavBar = () => {
     <NavigationMenu className="p-1 gap-2 items-start justify-start">
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Button variant="secondary" asChild>
+          <Button variant="ghost" asChild>
             <div className="flex flex-row">
-              <Image src="/icon.svg" width={32} height={32} alt="icon" />
               <Link href="/">Vitium</Link>
             </div>
           </Button>
