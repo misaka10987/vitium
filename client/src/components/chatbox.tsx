77314aa4aea1d3c623849056a8f77310dfbd9f4a
misaka10987 <misaka10987@outlook.com>
2025-04-23 13:46:28 +0800

    feature: add auto scroll to bottom for chat

--- client/src/components/chatbox.tsx
+++ client/src/components/chatbox.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect, useRef } from 'react'
 import { Textarea } from '@/components/ui/textarea'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { Chatbubble } from '@/components/chatbubble'
 import { Button } from '@/components/ui/button'
 import { sendMessage, setSSEListener } from '@/lib/chat'
@@ -23,6 +22,12 @@ export const Chatbox = () => {
   const { hostname } = useHostStore()
   const connectAttempts = useRef(0)
   const router = useRouter()
+  const container = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (container.current != null)
+      container.current.scrollTop = container.current?.scrollHeight
+  }, [messages])
 
   useEffect(() => {
     if (!hostname) return
@@ -47,7 +52,7 @@ export const Chatbox = () => {
 
       // Handle reconnection if needed
       eventSource.addEventListener('error', () => {
-        if (eventSource?.readyState === EventSource.CLOSED) {
+        if (eventSource?.readyState == EventSource.CLOSED) {
           connectAttempts.current += 1
           console.debug(
             `Connection closed. Attempt ${connectAttempts.current}/3`
@@ -71,60 +76,65 @@ export const Chatbox = () => {
   }, [hostname, router])
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <div className="h-[calc(100vh-180px)] flex w-full rounded-md border p-1 mb-2 overflow-scroll">
-        <div className="flex flex-col w-full">
+    <div className="flex flex-col-reverse h-full w-full gap-2">
+      <div className="flex w-full">
+        <form
+          ref={msgForm}
+          className="flex gap-2 items-center w-full"
+          onSubmit={(e) => {
+            e.preventDefault()
+            const form = new FormData(e.currentTarget)
+            const msg = form.get('msg')?.toString()
+            if (msg == undefined) throw Error()
+            sendMessage(msg)
+            if (msgInput?.current == null) throw Error()
+            msgInput.current.value = ''
+          }}
+        >
+          <Textarea
+            ref={msgInput}
+            name="msg"
+            className="h-full resize-none overflow-auto py-2"
+            placeholder="Type your message here..."
+            required
+            onKeyDown={(e) => {
+              if (e.key == 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+                msgForm.current?.requestSubmit()
+              }
+            }}
+          />
+          <div className="flex flex-col gap-1">
+            <Button
+              className="h-[40px] px-4"
+              type="submit"
+              aria-label="Send message"
+            >
+              <Send className="h-4 w-4" />
+            </Button>
+            <Button
+              className="h-[40px] px-4 mt-1"
+              type="button"
+              variant="outline"
+              aria-label="Upload image"
+            >
+              <Photo className="h-4 w-4" />
+            </Button>
+          </div>
+        </form>
+      </div>
+      <div className="flex flex-grow h-0 w-full rounded-md border">
+        <div
+          ref={container}
+          className="flex flex-col p-2 gap-1 w-full overflow-auto"
+        >
           {messages.map((msg, index) => (
-            <div className="flex w-full p-2" key={index}>
+            <div className="flex w-full" key={index}>
               <Chatbubble {...msg} />
             </div>
           ))}
         </div>
       </div>
-      <form
-        ref={msgForm}
-        className="flex gap-2 items-center"
-        onSubmit={(e) => {
-          e.preventDefault()
-          const form = new FormData(e.currentTarget)
-          const msg = form.get('msg')?.toString()
-          if (msg == undefined) throw Error()
-          sendMessage(msg)
-          if (msgInput?.current == null) throw Error()
-          msgInput.current.value = ''
-        }}
-      >
-        <Textarea
-          ref={msgInput}
-          name="msg"
-          className="h-[90px] w-full resize-none overflow-auto py-2"
-          placeholder="Type your message here..."
-          required
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
-              e.preventDefault()
-              msgForm.current?.requestSubmit()
-            }
-          }}
-        />
-        <div className="flex flex-col gap-1">
-          <Button
-            className="h-[40px] px-4"
-            type="submit"
-            aria-label="Send message"
-          >
-            <Send className="h-4 w-4" />
-          </Button>
-          <Button
-            className="h-[40px] px-4 mt-1"
-            type="button"
-            variant="outline"
-            aria-label="Upload image"
-          >
-            <Photo className="h-4 w-4" />
-          </Button>
-        </div>
-      </form>
     </div>
   )
 }
