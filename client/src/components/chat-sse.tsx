be7de690c84d7b6690fc19ee63f30b9c55ae3b2b
misaka10987 <misaka10987@outlook.com>
2025-04-29 17:52:03 +0800

    fix unintended closing sse stream

--- client/src/components/chat-sse.tsx
+++ client/src/components/chat-sse.tsx
@@ -1,19 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useHostStore } from './host'
 import { json } from 'typia'
 import { Message } from 'vitium-api'
 
 export function ChatSSE({ downstream }: { downstream: (_: Message) => void }) {
   const { host } = useHostStore()
+  const es = useRef<EventSource>(null)
+
   useEffect(() => {
     if (host == undefined) return
     const url = `https://${host}/api/chat`
-    const es = new EventSource(url)
-    es.addEventListener('message', (evt) => {
+    es.current = new EventSource(url)
+    return () => es.current?.close()
+  }, [host])
+
+  useEffect(() => {
+    if (es.current == null) return
+    es.current.onmessage = (evt) => {
       const data = json.assertParse<Message>(evt.data)
       downstream(data)
-    })
-    return () => es.close()
-  }, [host, downstream])
-  return undefined
+    }
+    return () => {
+      if (es.current != null) es.current.onmessage = () => {}
+    }
+  }, [es, downstream])
+
+  return null
 }
