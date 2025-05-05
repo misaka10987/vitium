8bd9d9bc4fc4f3af9b5e4a151ee93678ebb8e74e
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-05-05 15:43:46 +0800

    add docs

--- client/src/components/chatbox.tsx
+++ client/src/components/chatbox.tsx
@@ -3,30 +3,32 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
 import { CommandRecord, Message } from 'vitium-api'
 import { TurboInput } from '@/components/turbo-input'
-import { panic } from '@/lib/util'
+import { panic, defined } from '@/lib/util'
 import { Bubble } from './bubble'
 import { SSE } from './sse'
 import { json } from 'typia'
+import { useHostStore } from './host'
 
 export const Chatbox = () => {
   type Entry = Message | CommandRecord
   const [entries, setEntries] = useState<Entry[]>([])
 
   const container = useRef<HTMLDivElement>(null)
-
   useEffect(() => {
     const curr = container.current ?? panic()
     curr.scrollTop = curr.scrollHeight
   }, [entries])
 
-  const handle = useCallback((incoming: Entry) => setEntries((prev) => prev.concat([incoming])), [setEntries])
+  const { host } = useHostStore()
+
+  const handle = useCallback((incoming: Entry) => setEntries(prev => prev.concat([incoming])), [setEntries])
   const chat = useCallback((data: string) => handle(json.assertParse<Message>(data)), [handle])
   const cmd = useCallback((data: string) => handle(json.assertParse<CommandRecord>(data)), [handle])
 
   return (
     <div className="flex flex-col-reverse h-full w-full gap-2">
-      <SSE path="/api/chat" downstream={chat} />
-      <SSE path="/api/cmd" downstream={cmd} />
+      <SSE url={defined`https://${host}/api/chat`} downstream={chat} />
+      <SSE url={defined`https://${host}/api/cmd`} downstream={cmd} />
       <div className="flex w-full">
         <TurboInput />
       </div>
--- client/src/components/sse.tsx
+++ client/src/components/sse.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useRef } from 'react'
-import { useHostStore } from './host'
 
-export function SSE({ path, downstream }: { path: string, downstream: (_: string) => void }) {
-    const { host } = useHostStore()
+/**
+ * A React component for for handling an SSE endpoint.
+ * 
+ * Set `url` to `undefined` to shutdown the connection.
+ * 
+ * @param url the URL to subscribe to
+ * @param downstream downstream handler for received events
+ * @returns `null`
+ */
+export const SSE = ({ url, downstream }: { url?: string, downstream: (_: string) => void }) => {
     const es = useRef<EventSource>(null)
-    const url = `https://${host}${path}`
+
+    const close = () => {
+        es.current?.close()
+        es.current = null
+    }
 
     useEffect(() => {
-        if (host == undefined) return
-        es.current = new EventSource(url)
-        return () => es.current?.close()
-    }, [host, url])
+        if (url == undefined) close()
+        else es.current = new EventSource(url)
+        return close
+    }, [url])
 
     useEffect(() => {
         const handle = (evt: { data: string }) => downstream(evt.data)
