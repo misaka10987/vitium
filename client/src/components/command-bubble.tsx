9ea6a04ed941ec0ac9cdee2c2916f1246dcb8e66
misaka10987 <misaka10987@outlook.com>
2025-05-19 20:17:42 +0800

    implement ansi style display in command status

--- client/src/components/command-bubble.tsx
+++ client/src/components/command-bubble.tsx
@@ -2,31 +2,36 @@
 
 import { CommandRecord } from 'vitium-api'
 import { match, P } from 'ts-pattern'
+import ANSI from 'ansi-to-html'
 
 export const CommandBubble = ({
   record: [line, status],
 }: {
   record: CommandRecord
 }) => {
+  const ansi = new ANSI()
   const issuer = line.user ?? <span className="font-bold">Server</span>
   const [head, content] = match(status)
     .with({ Ok: P.string }, ({ Ok: status }) => [
       <span className="text-muted-foreground">
         {issuer}: <code>{line.line}</code>
       </span>,
-      status,
+      ansi.toHtml(status),
     ])
     .with({ Err: P.string }, ({ Err: status }) => [
       <span className="text-destructive">
         {issuer}: <code>{line.line}</code>
       </span>,
-      status,
+      ansi.toHtml(status),
     ])
     .exhaustive()
   return (
     <div className="flex-col text-sm items-center justify-center gap-2 w-full wrap-break-word m-1">
       <div className="align-middle">{head}</div>
-      <div>{content}</div>
+      <div
+        className="whitespace-pre-wrap"
+        dangerouslySetInnerHTML={{ __html: content.toString() }}
+      ></div>
     </div>
   )
 }
