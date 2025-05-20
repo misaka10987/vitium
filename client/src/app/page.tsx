b0fa8d6954d0ad1fc912e92be5dca4bbebc406b0
misaka10987 <misaka10987@outlook.com>
2025-05-20 09:51:07 +0800

    fix: optimize LCP

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
     <div className="flex h-full w-full align-top justify-center">
       <div className="flex flex-col gap-8 align-middle m-4">
         <div className="flex flex-col">
-          <Icon width={320} height={320} />
+          <Icon width={320} height={320} priority />
           <article>
             <h1 className="text-4xl font-bold text-center mb-4">Vitium</h1>
             <p className="text-center">The TRPG Framework</p>
@@ -22,7 +22,7 @@ export default function Page() {
         </div>
         <div className="flex justify-center">
           <Button asChild variant="secondary" className="m-4 select-none">
-            <Link href="/login">Player Login</Link>
+            <Link href="/login">Get Started</Link>
           </Button>
         </div>
       </div>
--- client/src/components/icon.tsx
+++ client/src/components/icon.tsx
@@ -1,21 +1,19 @@
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
-export const Icon = ({ width, height }: { width: number; height: number }) => {
+export const Icon = (props: Omit<ImageProps, 'src' | 'alt'>) => {
   return (
     <div className="select-none">
       <Image
         src="/icon-white.svg"
         alt="icon"
-        width={width}
-        height={height}
         className="hidden dark:inline"
+        {...props}
       />
       <Image
         src="/icon-black.svg"
         alt="icon"
-        width={width}
-        height={height}
         className="inline dark:hidden"
+        {...props}
       />
     </div>
   )
