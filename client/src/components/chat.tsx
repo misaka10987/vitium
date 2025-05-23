f4ed3a35d2828cf8e5468aeeebecd26cdf46b44a
misaka10987 <misaka10987@outlook.com>
2025-05-23 12:59:32 +0800

    use session storage to persist chat record

--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
 import { CommandRecord, Message } from 'vitium-api'
 import { TurboInput } from '@/components/turbo-input'
 import { panic, defined } from '@/lib/util'
@@ -8,37 +8,71 @@ import { Bubble, Entry } from './bubble'
 import { SSE } from './sse'
 import { json } from 'typia'
 import { useHostStore } from './host'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+/**
+ * React hook for accessing the chat record stored.
+ */
+export const useChatStore = create<{
+  /**
+   * The chat record.
+   */
+  chat: Entry[]
+
+  /**
+   * Add a new message to the end of chat record.
+   *
+   * @param entry the new message
+   */
+  addNew: (entry: Entry) => void
+
+  /**
+   * Clear the chat record, removing all entries.
+   */
+  clear: () => void
+}>()(
+  persist(
+    (set) => ({
+      chat: [],
+      addNew: (entry) => set(({ chat }) => ({ chat: [...chat, entry] })),
+      clear: () => set(() => ({ chat: [] })),
+    }),
+    {
+      name: 'chat',
+      // uses session storage
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+)
 
 /**
  * The chat column of the UI, including display and text input.
  */
 export const Chat = () => {
-  const [entries, setEntries] = useState<Entry[]>([])
+  const { chat, addNew } = useChatStore()
 
   const container = useRef<HTMLDivElement>(null)
   useEffect(() => {
     const curr = container.current ?? panic()
     curr.scrollTop = curr.scrollHeight
-  }, [entries])
+  }, [chat])
 
   const { host } = useHostStore()
 
-  const handle = useCallback(
-    (incoming: Entry) => setEntries((prev) => prev.concat([incoming])),
-    [setEntries]
+  const msg = useCallback(
+    (data: string) => addNew(json.assertParse<Message>(data)),
+    [addNew]
   )
-  const chat = useCallback(
-    (data: string) => handle(json.assertParse<Message>(data)),
-    [handle]
-  )
+
   const cmd = useCallback(
-    (data: string) => handle(json.assertParse<CommandRecord>(data)),
-    [handle]
+    (data: string) => addNew(json.assertParse<CommandRecord>(data)),
+    [addNew]
   )
 
   return (
     <div className="flex flex-col-reverse h-full w-full gap-2">
-      <SSE url={defined`https://${host}/api/chat`} downstream={chat} />
+      <SSE url={defined`https://${host}/api/chat`} downstream={msg} />
       <SSE url={defined`https://${host}/api/cmd`} downstream={cmd} />
       <div className="flex w-full">
         <TurboInput />
@@ -48,7 +82,7 @@ export const Chat = () => {
           ref={container}
           className="flex flex-col p-2 gap-2 w-full overflow-auto"
         >
-          {entries.map((entry, idx) => (
+          {chat.map((entry, idx) => (
             <div className="flex w-full" key={idx}>
               <Bubble entry={entry} />
             </div>
