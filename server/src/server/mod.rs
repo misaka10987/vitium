ef981a28c22a4a4fd3d5a8c7978e3a4348d3a9d9
misaka10987 <misaka10987@outlook.com>
2025-11-17 21:09:06 +0800

    implement basic client `<iframe>` embedding

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -7,16 +7,20 @@ mod profile;
 #[cfg(debug_assertions)]
 mod test;
 
+use anyhow::anyhow;
+use askama::Template;
 use axum::{
     Json, Router,
+    extract::State,
     http::StatusCode,
-    response::Redirect,
+    response::{Html, IntoResponse, Redirect},
     routing::{any, get},
 };
 use basileus::Basileus;
 use chat::ChatModule;
 use cmd::CommandModule;
 use http::HeaderValue;
+use local_ip_address::{local_ip, local_ipv6};
 use log::LogModule;
 use serde::{Deserialize, Serialize};
 use serde_inline_default::serde_inline_default;
@@ -25,19 +29,18 @@ use sqlx::{SqlitePool, query, sqlite::SqliteConnectOptions};
 use std::{
     collections::HashMap,
     error::Error,
+    iter::once,
     net::Ipv6Addr,
     ops::Deref,
     path::PathBuf,
-    sync::{Arc, atomic::AtomicBool},
+    sync::{Arc, OnceLock, atomic::AtomicBool},
     time::{Duration, SystemTime},
 };
 use tokio::{net::TcpListener, sync::RwLock};
-use tower_http::{
-    cors::{Any, CorsLayer},
-    trace::TraceLayer,
-};
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use tracing::{error, info};
 use url::Url;
+use urlencoding::Encoded;
 use vitium_api::user::UserProfile;
 
 const DB_INIT_QUERY: &'static str = r#"
@@ -55,12 +58,20 @@ pub struct ServerInst {
     pub cfg: Config,
     pub shutdown: ShutUp,
     started: AtomicBool,
+    port: OnceLock<u16>,
     db: SqlitePool,
     player: RwLock<HashMap<String, UserProfile>>,
     basileus: Basileus,
     chat: ChatModule,
     cmd: CommandModule,
     log: LogModule,
+}
+
+#[derive(Template)]
+#[template(path = "client.html")]
+struct Client {
+    pub url: Url,
+    pub comment: String,
 }
 
 /// Interface to the entire server.
@@ -89,6 +100,7 @@ impl Server {
             cfg,
             shutdown,
             started: AtomicBool::new(false),
+            port: OnceLock::new(),
             chat: ChatModule::new(db.clone()),
             db,
             player: RwLock::const_new(HashMap::new()),
@@ -97,6 +109,46 @@ impl Server {
             log,
         }));
         Ok(val)
+    }
+
+    /// Public URL of the HTTP server.
+    pub fn url(&self) -> anyhow::Result<Url> {
+        let host = if let Some(host) = &self.cfg.host {
+            host.clone()
+        } else {
+            let ip = local_ipv6().or(local_ip())?;
+            ip.to_string()
+        };
+        let port = if let Some(port) = &self.cfg.port {
+            port
+        } else {
+            self.port.get().ok_or(anyhow!("port not assigned yet"))?
+        };
+        let scheme = if self.cfg.https { "https" } else { "http" };
+        let url = Url::parse(&format!("{scheme}://{host}:{port}/")).unwrap();
+        Ok(url)
+    }
+
+    fn embed_client(
+        &self,
+        path: &str,
+        query: Option<HashMap<String, String>>,
+    ) -> anyhow::Result<String> {
+        let mut url = self.cfg.client.join(path).unwrap();
+        let query = query
+            .into_iter()
+            .flatten()
+            .chain(once(("server".into(), self.url()?.as_str().into())))
+            .map(|(k, v)| format!("{}={}", k, Encoded(v)))
+            .collect::<Vec<_>>()
+            .join("&");
+        url.set_query(Some(&query));
+        let client = Client {
+            url,
+            comment: format!("{}", self.cfg.client.join(path)?),
+        };
+        let html = client.render()?;
+        Ok(html)
     }
 
     pub fn started(&self) -> bool {
@@ -118,8 +170,6 @@ impl Server {
         CorsLayer::new()
             .allow_origin(origin.ascii_serialization().parse::<HeaderValue>().unwrap())
             .allow_credentials(true)
-            .allow_headers(Any)
-            .allow_methods(Any)
             .max_age(Duration::from_secs(3600))
     }
 
@@ -142,6 +192,7 @@ impl Server {
         let app = app.nest("/test", test::router());
 
         let app = app
+            .route("/", get(client_root))
             .route("/ping", get(|| async { Json(SystemTime::now()) }))
             .route("/hello", get("Hello, world!"))
             .route("/contact", get(Redirect::temporary(&self.cfg.contact)))
@@ -155,13 +206,20 @@ impl Server {
 
         let port = self.cfg.port.unwrap_or(0);
         let listener = TcpListener::bind((Ipv6Addr::UNSPECIFIED, port)).await?;
-        info!("listen {}", listener.local_addr()?);
+        let port = listener.local_addr()?.port();
+        self.port.set(port).unwrap();
+
+        info!("server available at {}", self.url()?);
 
         let shutdown = self.shutdown.clone();
+        let s = self.shutdown.child();
 
-        axum::serve(listener, app.with_state(self))
-            .with_graceful_shutdown(shutdown.wait())
-            .await?;
+        tokio::spawn(async move {
+            axum::serve(listener, app.with_state(self))
+                .with_graceful_shutdown(s.wait())
+                .await
+                .unwrap()
+        });
 
         Ok(shutdown)
     }
@@ -177,7 +235,12 @@ impl AsRef<Basileus> for Server {
 #[serde_inline_default]
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Config {
-    /// Port to start the API server on.
+    /// Hostname of the HTTP server.
+    /// If not specified, the local IP address would be used.
+    #[serde_inline_default(None)]
+    pub host: Option<String>,
+    /// Port of the HTTP server.
+    /// If not specified, would use the random port assigned by system.
     pub port: Option<u16>,
     /// Path to the server database.
     #[serde_inline_default("./server.db".into())]
@@ -194,6 +257,9 @@ pub struct Config {
     /// URL to contact the game server administrator, e.g. `mailto:example@example.org`.
     #[serde(default)]
     pub contact: String,
+    #[serde_inline_default(false)]
+    pub https: bool,
+    #[serde_inline_default(Url::parse("https://example.com/").unwrap())]
     pub client: Url,
 }
 
@@ -206,6 +272,8 @@ impl Default for Config {
             log: Default::default(),
             motd: String::new(),
             contact: Default::default(),
+            host: None,
+            https: true,
             client: Url::parse("https://example.com/").unwrap(),
         }
     }
@@ -214,4 +282,15 @@ impl Default for Config {
 fn internal_server_error(err: impl Error) -> StatusCode {
     error!("{err}");
     StatusCode::INTERNAL_SERVER_ERROR
+}
+
+async fn client_root(State(s): State<Server>) -> Result<Html<String>, StatusCode> {
+    let client = match s.embed_client("/", None) {
+        Ok(html) => html,
+        Err(e) => {
+            error!("{e}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+    Ok(Html(client))
 }
