e978d201129234e21012c84a6e1b36460f3621a3
matthew1115 <matthew_1115@outlook.com>
2025-12-03 11:22:24 +0800

    add packages and add dark mode

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,13 +4,28 @@ import { Suspense } from 'solid-js'
 import Nav from '~/components/Nav'
 import './app.css'
 
+import { isServer } from "solid-js/web"
+ 
+import { ColorModeProvider, ColorModeScript, cookieStorageManagerSSR } from "@kobalte/core"
+import { getCookie } from "vinxi/http"
+ 
+function getServerCookies() {
+  "use server"
+  const colorMode = getCookie("kb-color-mode")
+  return colorMode ? `kb-color-mode=${colorMode}` : ""
+}
+ 
 export default function App() {
+  const storageManager = cookieStorageManagerSSR(isServer ? getServerCookies() : document.cookie)
   return (
     <Router
       root={(props) => (
         <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
+          <ColorModeScript storageType={storageManager.type} />
+          <ColorModeProvider storageManager={storageManager}>
+            <Nav />
+            <Suspense>{props.children}</Suspense>
+          </ColorModeProvider>
         </>
       )}
     >
