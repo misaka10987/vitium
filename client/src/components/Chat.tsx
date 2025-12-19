4fac750635489c1da7a84be649ec3c235794e60e
matthew1115 <matthew_1115@outlook.com>
2025-12-19 14:41:31 +0800

    add solid devtools and a few more example messages

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -15,6 +15,14 @@ export const Chat = () => {
     { id: 2, author: 'Guide', content: 'Type a command or send a message below.' },
     // Example HTML-enabled message:
     { id: 3, author: 'Admin', content: '<b>Hello</b> <i>world</i>!', htmlEnabled: true },
+    { id: 4, author: 'User', content: 'This is a plain text message.' },
+    { id: 5, author: 'Moderator', content: '<script>alert("XSS Attack!")</script> This should be safe.', htmlEnabled: true },
+    { id: 6, author: 'User2', content: 'Just another message.' },
+    { id: 7, author: 'User3', content: '<div style="color: red;">This is red text.</div>', htmlEnabled: true },
+    { id: 8, author: 'User4', content: 'Goodbye!' },
+    { id: 9, author: 'System', content: 'yet another system message' },
+    { id: 10, author: 'Guide', content: 'Notice that the commands start with \'/\' and is not documented' },
+    { id: 11, author: 'Admin', content: '<u>Underlined text</u> is also supported.', htmlEnabled: true },
   ]
 
   return (
--- client/src/entry-client.tsx
+++ client/src/entry-client.tsx
@@ -1,4 +1,6 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
+import 'solid-devtools' // for development only
+
 mount(() => <StartClient />, document.getElementById("app")!);
