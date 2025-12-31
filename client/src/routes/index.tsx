18caf2cc8d8a97b74e601ac28f0bb88025b5c6d1
matthew1115 <matthew_1115@outlook.com>
2025-12-31 09:09:35 +0800

    more fixes on the index page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -34,7 +34,8 @@ export default function Home() {
   return (
     <main class="flex flex-row items-center flex-1 pb-24 px-20 bg-background text-foreground min-h-screen select-none">
       <div class="flex flex-col items-center flex-1 select-none">
-        <div class="text-6xl font-semibold mb-6 text-center select-none">Vitium: A TRPG Framework</div>
+        <div class="text-7xl font-semibold mb-6 text-center select-none">Vitium</div>
+        <div class="text-lg mb-8 select-none">A TRPG Framework</div>
         <div class="flex flex-row">
           <A href="/login" class="select-none mx-2">
             <Button size="lg" variant="default" class="text-lg select-none rounded-full">
@@ -42,16 +43,16 @@ export default function Home() {
               Login
             </Button>
           </A>
-          <A href="/signin" class="select-none mx-2">
+          <A href="/signup" class="select-none mx-2">
             <Button size="lg" variant="outline" class="text-lg select-none rounded-full">
               <UserPlus class="w-6 h-6" />
-              Signin
+              Sign Up
             </Button>
           </A>
         </div>
       </div>
       <div class="flex flex-col items-center flex-1 select-none">
-        <img src={iconSrc()} alt="Vitium" class="w-96 h-96 select-none" />
+        <img src={iconSrc()} alt="Vitium" class="w-full h-full select-none" />
       </div>
     </main>
   );
