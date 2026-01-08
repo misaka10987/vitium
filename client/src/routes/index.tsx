369bebe75efe830a9a97e3c9547f55161deb482d
matthew1115 <matthew_1115@outlook.com>
2026-01-09 07:55:57 +0800

    refactor: update layout in index.tsx for new narrow viewpoint with icon on top

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,8 +5,8 @@ import Icon from "~/asset/icon.svg"
 
 export default function Home() {
   return (
-    <main class="grid grid-cols-1 items-center h-full mx-24 md:gap-16 md:grid-cols-2 bg-background text-foreground">
-      <div class="flex flex-col items-center">
+    <main class="flex flex-col-reverse items-center justify-center h-full mx-24 md:gap-16 md:flex-row bg-background text-foreground">
+      <div class="flex flex-col items-center md:max-w-1/2">
         <div class="text-5xl md:text-6xl font-medium mb-6 text-center">Vitium: A TRPG Framework</div>
         <div class="text-lg mb-8">Login to start your adventure</div>
         <div class="flex flex-row">
@@ -24,7 +24,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div class="flex items-center justify-center [@media(max-height:48rem)_and_(max-width:767.98px)]:hidden select-none">
+      <div class="flex items-center justify-center [@media(max-height:40rem)_and_(max-width:767.98px)]:hidden select-none">
         <img src={Icon} class="w-72 h-72 md:w-96 md:h-96 min-w-64 select-none" alt="Vitium Logo" />
       </div>
     </main>
