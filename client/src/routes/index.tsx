ea797fae1e6421b6873d1c780f4ffc7871edb622
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-01-20 18:15:00 +0800

    cease abuse of `select-none`

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,32 +1,38 @@
-import { Button } from "~/components/ui/button";
-import LogIn from "lucide-solid/icons/log-in";
-import UserPlus from "lucide-solid/icons/user-plus";
-import Icon from "~/asset/icon.svg"
+import { Button } from '~/components/ui/button'
+import LogIn from 'lucide-solid/icons/log-in'
+import UserPlus from 'lucide-solid/icons/user-plus'
+import Icon from '~/asset/icon.svg'
 
 export default function Home() {
   return (
     <main class="flex flex-col-reverse items-center justify-center h-full mx-24 md:gap-16 md:flex-row bg-background text-foreground">
       <div class="flex flex-col items-center md:max-w-1/2">
-        <div class="text-5xl md:text-6xl font-medium mb-6 text-center">Vitium: A TRPG Framework</div>
+        <div class="text-5xl md:text-6xl font-medium mb-6 text-center">
+          Vitium: A TRPG Framework
+        </div>
         <div class="text-lg mb-8">Login to start your adventure</div>
         <div class="flex flex-row">
-          <a href="/login" class="select-none mx-2">
-            <Button size="lg" variant="default" class="text-lg select-none rounded-full">
+          <a href="/login" class="mx-2">
+            <Button size="lg" variant="default" class="text-lg rounded-full">
               <LogIn class="w-6 h-6" />
               Login
             </Button>
           </a>
-          <a href="/signup" class="select-none mx-2">
-            <Button size="lg" variant="outline" class="text-lg select-none rounded-full">
+          <a href="/signup" class=" mx-2">
+            <Button size="lg" variant="outline" class="text-lg rounded-full">
               <UserPlus class="w-6 h-6" />
               Sign Up
             </Button>
           </a>
         </div>
       </div>
-      <div class="flex items-center justify-center [@media(max-height:40rem)_and_(max-width:767.98px)]:hidden select-none">
-        <img src={Icon} class="w-72 h-72 md:w-96 md:h-96 min-w-64 select-none" alt="Vitium Logo" />
+      <div class="flex items-center justify-center [@media(max-height:40rem)_and_(max-width:767.98px)]:hidden">
+        <img
+          src={Icon}
+          class="w-72 h-72 md:w-96 md:h-96 min-w-64"
+          alt="Vitium Logo"
+        />
       </div>
     </main>
-  );
+  )
 }
