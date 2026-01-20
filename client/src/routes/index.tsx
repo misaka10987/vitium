0a7ebf4ec5bbd0d96e148b17270c880e8d088bef
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-01-20 20:34:32 +0800

    refactor: use `flex` and `flex-wrap` for layout

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,34 +5,32 @@ import Icon from '~/asset/icon.svg'
 
 export default function Home() {
   return (
-    <main class="flex flex-col-reverse items-center justify-center h-full mx-24 md:gap-16 md:flex-row bg-background text-foreground">
-      <div class="flex flex-col items-center md:max-w-1/2 gap-8">
-        <div class="flex flex-col items-center gap-4">
-          <div class="text-4xl font-semibold text-center">
-            Vitium: A TRPG Framework
+    <main class="w-full h-full flex items-center justify-center overflow-scroll">
+      <div class="flex flex-wrap-reverse items-center justify-center gap-12 m-12">
+        <div class="flex flex-col gap-8">
+          <div class="flex flex-col gap-4">
+            <div class="text-6xl font-semibold">TRPG Framework</div>
+            <div class="text-lg text-secondary-foreground p-1">
+              Login to start your adventure
+            </div>
           </div>
-          <div class="text-lg">Login to start your adventure</div>
+          <div class="flex flex-row">
+            <a href="/login" class="mx-2">
+              <Button size="lg" variant="default" class="text-lg rounded-full">
+                <LogIn class="w-6 h-6" />
+                Login
+              </Button>
+            </a>
+            <a href="/signup" class="mx-2">
+              <Button size="lg" variant="outline" class="text-lg rounded-full">
+                <UserPlus class="w-6 h-6" />
+                Sign Up
+              </Button>
+            </a>
+          </div>
         </div>
-        <div class="flex flex-row">
-          <a href="/login" class="mx-2">
-            <Button size="lg" variant="default" class="text-lg rounded-full">
-              <LogIn class="w-6 h-6" />
-              Login
-            </Button>
-          </a>
-          <a href="/signup" class="mx-2">
-            <Button size="lg" variant="outline" class="text-lg rounded-full">
-              <UserPlus class="w-6 h-6" />
-              Sign Up
-            </Button>
-          </a>
-        </div>
+        <img src={Icon} class="w-64" alt="Vitium Logo" />
       </div>
-      <img
-        src={Icon}
-        class="w-72 h-72 md:w-96 md:h-96 min-w-64"
-        alt="Vitium Logo"
-      />
     </main>
   )
 }
