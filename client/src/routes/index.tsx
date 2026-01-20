09a6eaffa4b643c4930d6e879901e247abbf4406
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-01-20 20:52:40 +0800

    improve HTML sementicity

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -9,24 +9,32 @@ export default function Home() {
       <div class="flex flex-wrap-reverse items-center justify-center gap-12 m-12">
         <div class="flex flex-col gap-8">
           <div class="flex flex-col gap-4">
-            <div class="text-6xl font-semibold">TRPG Framework</div>
-            <div class="text-lg text-secondary-foreground p-1">
+            <h1 class="text-6xl font-semibold">TRPG Framework</h1>
+            <p class="text-lg text-secondary-foreground p-1">
               Login to start your adventure
-            </div>
+            </p>
           </div>
-          <div class="flex flex-row">
-            <a href="/login" class="mx-2">
-              <Button size="lg" variant="default" class="text-lg rounded-full">
-                <LogIn class="w-6 h-6" />
-                Login
-              </Button>
-            </a>
-            <a href="/signup" class="mx-2">
-              <Button size="lg" variant="outline" class="text-lg rounded-full">
-                <UserPlus class="w-6 h-6" />
-                Sign Up
-              </Button>
-            </a>
+          <div class="flex flex-row gap-4 p-1">
+            <Button
+              as="a"
+              href="/login"
+              size="lg"
+              variant="default"
+              class="text-lg rounded-full"
+            >
+              <LogIn class="w-6 h-6" />
+              Start
+            </Button>
+            <Button
+              as="a"
+              href="/signup"
+              size="lg"
+              variant="outline"
+              class="text-lg rounded-full"
+            >
+              <UserPlus class="w-6 h-6" />
+              Sign Up
+            </Button>
           </div>
         </div>
         <img src={Icon} class="w-64" alt="Vitium Logo" />
